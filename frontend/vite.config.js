058541c0aeab01e0fe8f604a4246fb7989316a0e
cheedeti pavan reddy <cheedetipavanreddy@gmail.com>
2025-10-09 15:57:48 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: '.', // default is project root
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './frontend/index.html', // specify index.html location
-    },
-  },
   plugins: [react()],
+  root: './',       // root is frontend folder itself
+  build: {
+    outDir: 'dist', // output folder for build
+  },
 });
