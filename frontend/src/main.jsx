6c9288e875ce36bc765604c8b0c951a744213c66
cheedeti pavan reddy <cheedetipavanreddy@gmail.com>
2025-10-09 16:34:30 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,10 @@
-// src/main.jsx
-import App from './App.jsx'; // <-- must match actual file name
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/globals.css";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
