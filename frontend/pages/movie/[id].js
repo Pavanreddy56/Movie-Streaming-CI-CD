99daf6882e09468ac9284c7e5d122ee07f78521e
cheedeti pavan reddy <cheedetipavanreddy@gmail.com>
2025-10-09 16:14:12 +0530

    Update [id].js

--- frontend/pages/movie/[id].js
+++ frontend/pages/movie/[id].js
@@ -1,1 +1,15 @@
-console.log('Movie page');
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
+import MoviePage from "./pages/movie/[id]"; // only if needed directly
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/movie/:id" element={<MoviePage />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
