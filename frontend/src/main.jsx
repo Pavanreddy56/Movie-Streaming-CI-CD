726c3390907f7f01afc0023bfde158f1f03eb486
cheedeti pavan reddy <cheedetipavanreddy@gmail.com>
2025-10-09 16:16:05 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/movie"; // Example
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import App from "./App";
+import MoviePage from "./pages/movie/[id]"; // direct import of dynamic page
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/movie/:id" element={<MoviePage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
+
